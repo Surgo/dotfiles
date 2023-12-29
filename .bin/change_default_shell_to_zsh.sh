5c6ed1a6bfc4e71d36ca601d821567c953a87e5f
Kosei Kitahara <surgo.jp@gmail.com>
2023-12-30 04:14:27 +0900

    Update path

--- .bin/change_default_shell_to_zsh.sh
+++ .bin/change_default_shell_to_zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo -i bash << EOF
-echo "/usr/local/bin/zsh" >> /etc/shells
+echo "/opt/homebrew/bin/zsh" >> /etc/shells
 EOF
-chsh -s /usr/local/bin/zsh
+chsh -s /opt/homebrew/bin/zsh
