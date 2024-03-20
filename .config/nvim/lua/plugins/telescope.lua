3508fb19a1264ce0b938f0a16a9a18aeef033ce1
Kosei Kitahara <surgo.jp@gmail.com>
2024-03-21 03:45:51 +0900

    Install telescope-file-browser

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -25,3 +25,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+require("telescope").load_extension "file_browser"
