210c67fd41f9829e7e3765a3c9dfd2d2949ed809
Kosei Kitahara <surgo.jp@gmail.com>
2024-03-21 03:31:53 +0900

    Install telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,27 @@
+---- telescope.nvim
+local actions = require("telescope.actions")
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = { ["<esc>"] = actions.close },
+      n = { ["q"] = actions.close },
+    },
+    vimgrep_arguments = {
+      "ag",
+      "--nocolor",
+      "--noheading",
+      "--numbers",
+      "--column",
+      "--smart-case",
+      "--silent",
+      "--vimgrep",
+    }
+  }
+}
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', ';;', builtin.buffers, {})
+vim.keymap.set('n', '::', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
