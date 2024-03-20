4f2787459306b87695cd2948c2bbf09d007a6aaa
Kosei Kitahara <surgo.jp@gmail.com>
2024-03-21 03:27:36 +0900

    Kickstart by kickstart.nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- Reffered:
+-- - https://github.com/nvim-lua/kickstart.nvim
+
+require("base")
+require("options")
+require("keymaps")
+require("autocmds")
+require("colorscheme")
---
+++ .config/nvim/lua/base.lua
@@ -0,0 +1,6 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = true
