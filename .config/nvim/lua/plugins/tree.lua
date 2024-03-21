3aceeb5c9822bb95dd06aed15dd83962941bec9b
Kosei Kitahara <surgo.jp@gmail.com>
2024-03-22 04:09:44 +0900

    Install nvim-tree

---
+++ .config/nvim/lua/plugins/tree.lua
@@ -0,0 +1,35 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- showing the tree of my current buffer from where i open up nvim-tree
+vim.g.nvim_tree_respect_buf_cwd = 1
+
+require("nvim-tree").setup({
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  renderer = {
+    highlight_git = true,
+    highlight_opened_files = 'name',
+    icons = {
+      glyphs = {
+        git = {
+          unstaged = '!', renamed = '»',
+          untracked = '?', deleted = '✘',
+          staged = '✓', unmerged = '', ignored = '◌',
+        },
+      },
+    },
+  },
+  git = {
+    enable = true,
+    ignore = false,
+  },
+})
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
