7a88a2632ab5cbb735da81bbb9103a000240973f
Kosei Kitahara <surgo.jp@gmail.com>
2024-03-22 01:47:43 +0900

    Install LSP and mason tools

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,11 @@ require("autocmds")
 require("colorscheme")
 -- Start plugins
 require("plugins.web-devicons")
-require("plugins.copilot")
 require("plugins.lualine")
 require("plugins.telescope")
-require("plugins.treesitter")
+require("plugins.copilot")
+require("plugins.fidget")
+-- require("plugins.treesitter") # Not works with telescope
+require("plugins.mason-lspconfig")
+require("plugins.mason-tool-installer")
+require("plugins.lspconfig")
---
+++ .config/nvim/lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,5 @@
+require('mason-tool-installer').setup({
+  auto_update = true,
+  start_delay = 1000,
+  debounce_hours = 4,
+})
