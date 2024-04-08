7552183857ec1c50eafe0e5aa43fbeb1e57074b9
Kosei Kitahara <surgo.jp@gmail.com>
2024-04-08 12:24:18 +0900

    Run ban's build

---
+++ .bin/build_bat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+
+bat cache --build
--- .bin/init.sh
+++ .bin/init.sh
@@ -3,3 +3,5 @@
 "$(dirname "$0")/install_brew.sh"
 "$(dirname "$0")/configure_mac.sh"
 "$(dirname "$0")/change_default_shell_to_zsh.sh"
+
+"$(dirname "$0")/update.sh"
