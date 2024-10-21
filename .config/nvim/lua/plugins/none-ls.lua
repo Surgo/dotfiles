0ebce4e9aebe32d23e9c86bae85bd4d9010d5932
Kosei Kitahara <surgo.jp@gmail.com>
2024-10-21 12:17:37 +0900

    Auto format on save

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,10 +1,25 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = "lsp_format_on_save",
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = "lsp_format_on_save",
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
+    end
+  end,
   diagnostics_format = "[#{m}] #{s} (#{c})",
   sources = {
     null_ls.builtins.diagnostics.sqlfluff.with({
       extra_args = { "--dialect", "postgres" },
     }),
-  }
+  },
 })
