f32d3e97815c08f96f638446b8514bf8e7e52703
Kosei Kitahara <surgo.jp@gmail.com>
2024-11-12 19:18:12 +0900

    Notify to fidget

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,14 +5,25 @@ local null_ls_format_on_save = vim.api.nvim_create_augroup("NullLsFormatOnSave",
 null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") and client.name == "null-ls" then
-      print(string.format("[null-ls] [%s] Enable auto-format on save", client.name))
+      local fidget = require("fidget")
+      fidget.notify(string.format("[null-ls] [%s] Enable auto-format on save", client.name))
       vim.api.nvim_clear_autocmds({ group = null_ls_format_on_save, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = null_ls_format_on_save,
         buffer = bufnr,
-        callback = function()
-          print(string.format("[null-ls] formatted", client.name))
-          vim.lsp.buf.format({ async = false })
+        callback = function(c)
+          fidget.notify("[null-ls] formatted")
+          local allow_format = function(servers)
+            return function(filter_client)
+              return vim.tbl_contains(servers, filter_client.name)
+            end
+          end
+          vim.lsp.buf.format({
+            filter = allow_format({
+              "null-ls",
+            }),
+            async = false,
+          })
         end,
       })
     end
