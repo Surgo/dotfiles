45dcb747114603bed4f8cd81e67e034e86e96eda
Kosei Kitahara <surgo.jp@gmail.com>
2024-11-25 11:39:48 +0900

    Refactor LSP configs

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -8,3 +8,22 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+vim.g.format_on_save_enabled = true
+
+local toggle_format_on_save = function()
+	local fidget = require("fidget")
+
+	vim.g.format_on_save_enabled = not vim.g.format_on_save_enabled
+	if vim.g.format_on_save_enabled then
+		fidget.notify("[null-ls] Auto-format on save ENABLED")
+	else
+		fidget.notify("[null-ls] Auto-format on save DISABLED")
+	end
+end
+vim.api.nvim_create_user_command("ToggleFormatOnSave", toggle_format_on_save, {})
+
+local format = function()
+	vim.lsp.buf.format({ async = false })
+end
+vim.api.nvim_create_user_command("Format", format, {})
