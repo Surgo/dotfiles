47317875e1f2fdd879822e4d6a4a8eecd4ae49ba
Kosei Kitahara <surgo.jp@gmail.com>
2025-01-20 10:31:08 +0900

    Disable global

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -3,9 +3,6 @@ local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
 local sources = {
-	diagnostics.sqlfluff.with({
-		extra_args = { "--dialect", "postgres" },
-	}),
 	formatting.prettier.with({
 		prefer_local = true,
 	}),
